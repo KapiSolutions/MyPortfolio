582f8355e2baadeefdb52913072009d25cc00e43
JKapturkiewicz <kuba.kapek@gmail.com>
2023-04-18 19:13:13 +0200

    Add new types to the Project interface

--- interfaces/projects.ts
+++ interfaces/projects.ts
@@ -1,21 +1,24 @@
 type Lang = {
   en: string;
   pl: string;
-}
+};
 export type Locale = keyof Lang;
 
 type Images = {
   images: Array<string>;
-}
+};
 
 export interface Project {
+  id: string;
   title: Lang;
+  shortDesc: Lang;
   description: Lang;
   background: Lang & Images;
   process: Lang & Images;
   features: Lang & Images;
   outcome: Lang & Images;
   conclusion: Lang & Images;
+  clientReview: Lang & { name: string };
   asFreelancer: boolean;
   technology: Array<string>;
   liveLink: string;

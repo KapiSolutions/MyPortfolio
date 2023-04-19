d5d6619baadba974a304c27323e0f86cf565c58a
JKapturkiewicz <kuba.kapek@gmail.com>
2023-04-19 18:14:49 +0200

    Add version of the switch for the mobile devices

--- components/LocaleSwitch.tsx
+++ components/LocaleSwitch.tsx
@@ -1,24 +1,51 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { Box, Stack, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { useRouter } from "next/router";
 import { Locale } from "@/interfaces/main";
+import MultipleStopIcon from "@mui/icons-material/MultipleStop";
 
 export const LocaleSwitch = () => {
   const router = useRouter();
   const locale = (router.locale || "en") as Locale;
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"), {
+    defaultMatches: true,
+  });
   const toggleLocale = (newLocale: string) => {
     const { pathname, asPath, query } = router;
     router.push({ pathname, query }, asPath, { locale: newLocale });
   };
   return (
-    <Typography
-      variant="button"
-      className="pointer"
-      onClick={() => {
-        toggleLocale(locale === "en" ? "pl" : "en");
-      }}
-    >
-      {locale === "en" ? "pl" : "en"}
-    </Typography>
+    <>
+      {isMobile ? (
+        <Stack
+          direction="row"
+          justifyContent="left"
+          alignItems="center"
+          spacing={2}
+          onClick={() => {
+            toggleLocale(locale === "en" ? "pl" : "en");
+          }}
+        >
+          <Typography variant="button" className="pointer">
+            {locale === "en" ? "en" : "pl"}
+          </Typography>
+          <MultipleStopIcon />
+          <Typography variant="button" className="pointer">
+            {locale === "en" ? "pl" : "en"}
+          </Typography>
+        </Stack>
+      ) : (
+        <Typography
+          variant="button"
+          className="pointer"
+          onClick={() => {
+            toggleLocale(locale === "en" ? "pl" : "en");
+          }}
+        >
+          {locale === "en" ? "pl" : "en"}
+        </Typography>
+      )}
+    </>
   );
 };

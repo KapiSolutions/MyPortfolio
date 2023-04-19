8e168cbcbf95dd98c0d594e8ee67c8d07e05260e
JKapturkiewicz <kuba.kapek@gmail.com>
2023-04-19 11:01:43 +0200

    Add translations and LocaleSwitch

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,25 +1,41 @@
 import React, { useState } from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
 import ThemeSwitch from "../ThemeSwitch";
 import Link from "next/link";
+import {
+  Divider,
+  Stack,
+  Menu,
+  MenuItem,
+  Button,
+  Container,
+  Typography,
+  useMediaQuery,
+  IconButton,
+  Toolbar,
+  Box,
+  AppBar,
+  useTheme,
+} from "@mui/material";
+import { Locale } from "@/interfaces/main";
+import { LocaleSwitch } from "../LocaleSwitch";
 
-const pages = ["My Works", "About Me"];
+//Define Types:
+type Props = {
+  locale: Locale;
+};
 
-function Navbar() {
+const Navbar = ({ locale }: Props): JSX.Element => {
   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
-
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"), {
+    defaultMatches: true,
+  });
+  const pages = {
+    en: ["My Works", "About Me"],
+    pl: ["Projekty", "O mnie"],
+  };
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
   };
@@ -33,7 +49,7 @@ function Navbar() {
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
-          <Link href="/"  >
+          <Link href="/">
             <Typography
               variant="h6"
               noWrap
@@ -81,7 +97,7 @@ function Navbar() {
                 display: { xs: "block", md: "none" },
               }}
             >
-              {pages.map((page) => (
+              {pages[locale].map((page) => (
                 <MenuItem key={page} onClick={handleCloseNavMenu}>
                   <Typography textAlign="center">{page}</Typography>
                 </MenuItem>
@@ -108,17 +124,29 @@ function Navbar() {
             JK
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
+            {pages[locale].map((page) => (
               <Button key={page} onClick={handleCloseNavMenu} sx={{ my: 2, color: "white", display: "block" }}>
                 {page}
               </Button>
             ))}
           </Box>
           {/* ThemeSwitch */}
-          <ThemeSwitch />
+          <Stack
+            direction="row"
+            justifyContent="right"
+            alignItems="center"
+            spacing={2}
+            divider={<Divider orientation="vertical" flexItem />}
+            component="nav"
+          >
+            <Box sx={{ transform: isMobile ? "none" : "translateX(5px)" }}>
+              <ThemeSwitch />
+            </Box>
+            {isMobile ? null : <LocaleSwitch />}
+          </Stack>
         </Toolbar>
       </Container>
     </AppBar>
   );
-}
+};
 export default Navbar;

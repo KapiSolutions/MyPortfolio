9118bbeffc891a26ab7635052128fe748fa40989
JKapturkiewicz <kuba.kapek@gmail.com>
2023-04-21 14:40:06 +0200

    Add separator for the study type of item

--- components/sections/carrier/CarrierItem.tsx
+++ components/sections/carrier/CarrierItem.tsx
@@ -11,6 +11,12 @@ import SchoolIcon from "@mui/icons-material/School";
 import WorkIcon from "@mui/icons-material/Work";
 import DoneAllIcon from "@mui/icons-material/DoneAll";
 import AddToQueueIcon from "@mui/icons-material/AddToQueue";
+import ArchitectureIcon from '@mui/icons-material/Architecture';
+import EjectIcon from '@mui/icons-material/Eject';
+import FactoryIcon from '@mui/icons-material/Factory';
+import MilitaryTechIcon from '@mui/icons-material/MilitaryTech';
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
+import PrecisionManufacturingIcon from '@mui/icons-material/PrecisionManufacturing';
 import { Typography } from "@mui/material";
 import { Job } from "@/interfaces/carrier";
 import { Locale } from "@/interfaces/main";
@@ -21,6 +27,14 @@ type Props = {
   locale: Locale;
 };
 export const CarrierItem = ({ item, locale }: Props): JSX.Element => {
+    const t = {
+        en: {
+          now: "Now",
+        },
+        pl: {
+          now: "Teraz",
+        },
+      };
   const getSeparator = (type: string) => {
     switch (type) {
       case "freelancer":
@@ -48,9 +62,19 @@ export const CarrierItem = ({ item, locale }: Props): JSX.Element => {
           <TimelineSeparator>
             <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
             <TimelineDot color="secondary">
-              <SchoolIcon />
+              <FactoryIcon />
             </TimelineDot>
             <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
+          </TimelineSeparator>
+        );
+        case "study":
+        return (
+          <TimelineSeparator>
+            <TimelineConnector />
+            <TimelineDot color="warning">
+              <SchoolIcon />
+            </TimelineDot>
+            <TimelineConnector />
           </TimelineSeparator>
         );
       default:
@@ -61,15 +85,29 @@ export const CarrierItem = ({ item, locale }: Props): JSX.Element => {
         );
     }
   };
+
+  const timePeriod = (start: string, end: string) => {
+    // console.log(start, end);
+    if (end) {
+      if (start.localeCompare(end) === 0) {
+        return start;
+      } else {
+        return `${start} - ${end}`;
+      }
+    }else
+    {
+      return `${start} - ${t[locale].now}`;
+    }
+  };
   return (
     <TimelineItem>
       <TimelineOppositeContent sx={{ m: "auto 0" }} variant="body2" color="text.secondary">
-        {item.dateEnd ? `${item.dateStart} - ${item.dateEnd}` : `${item.dateStart} -`}
+        {timePeriod(item.dateStart, item.dateEnd)}
       </TimelineOppositeContent>
       {getSeparator(item.type)}
       <TimelineContent sx={{ py: "12px" }}>
         <Typography variant="h6" component="span">
-          {item.companyName}
+          {item.companyName[locale]}
         </Typography>
         <Typography variant="caption" component="p">
           {item.as[locale]}

8496b5d748ca1234257eb4559c516a8d9539df88
JKapturkiewicz <kuba.kapek@gmail.com>
2023-04-26 18:16:48 +0200

    Add Fusion360 iframe

--- components/sections/BookBoxSection.tsx
+++ components/sections/BookBoxSection.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useRouter } from "next/router";
-import { Box, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import { Locale } from "@/interfaces/main";
 
 const BookBoxSection = (): JSX.Element => {
@@ -8,15 +8,22 @@ const BookBoxSection = (): JSX.Element => {
   const locale = (router.locale || "en") as Locale;
   const t = {
     en: {
-      h: "BookBox Section",
+      h: "BookBox",
     },
     pl: {
-      h: "BookBox Section",
+      h: "BookBox",
     },
   };
   return (
-    <Box sx={{width: "100vw", height: "90vh",backgroundColor: "divider", p:2}}>
-      <Typography variant="h2">{t[locale]?.h}</Typography>
+    <Box sx={{ width: "100vw", height: "90vh", backgroundColor: "divider" }}>
+      <Container>
+        <Typography variant="h2">{t[locale]?.h}</Typography>
+        <iframe
+          src="https://myhub.autodesk360.com/ue290b326/shares/public/SHd38bfQT1fb47330c99108e411f643e6476?mode=embed"
+          allowFullScreen={true}
+          style={{ height: "75vh",width: "100%",border: "none", margin: "auto" }}
+        ></iframe>
+      </Container>
     </Box>
   );
 };

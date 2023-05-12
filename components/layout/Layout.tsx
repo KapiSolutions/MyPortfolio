52dbeeb40693377b2baacef2ccd9b457b30d04a4
JKapturkiewicz <kuba.kapek@gmail.com>
2023-05-12 13:06:24 +0200

    Add ThemeProvider and Redux appSelector for the themeSate reading purpose

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useMemo } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import styles from "../../styles/layout/grid.module.scss";
 import { useRouter } from "next/router";
 import { Locale } from "@/interfaces/main";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import paletteProvider from "@/utils/paletteProvider";
+import { useAppSelector } from "../../store/hooks";
 
 //Define Types:
 type Props = {
@@ -13,14 +17,19 @@ type Props = {
 const Layout = ({ children }: Props): JSX.Element => {
   const router = useRouter();
   const locale = (router.locale || "en") as Locale;
+  const themeState = useAppSelector((state) => state.device.theme);
+  const theme = useMemo(() => createTheme(paletteProvider(themeState)), [themeState]);
   return (
-    <div className={styles.container}>
-      <Header locale={locale} />
-      <main name="main" className={styles.main}>
-        {children}
-      </main>
-      <Footer locale={locale} />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className={styles.container}>
+        <Header locale={locale} />
+        <main name="main" className={styles.main}>
+          {children}
+        </main>
+        <Footer locale={locale} />
+      </div>
+    </ThemeProvider>
   );
 };
 

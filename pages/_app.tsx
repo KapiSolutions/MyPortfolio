2db0f7ff572ea8c0ece9b08125586514367ec826
JKapturkiewicz <kuba.kapek@gmail.com>
2023-05-12 13:02:57 +0200

    Add Redux provider and move ThemeProvider to the Layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,19 @@
-import React, { useMemo } from "react";
+import React from "react";
 import "@/styles/main.scss";
 import "@fontsource/k2d";
 import Layout from "../components/layout/Layout";
-import { ThemeProvider, useTheme, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import { useDeviceStore } from "@/stores/deviceStore";
-import paletteProvider from "@/utils/paletteProvider";
+import { Provider } from "react-redux";
+import { store } from "../store/store";
+
 // Import types
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const themeState = useDeviceStore((state) => state.themeState);
-  // Update the theme only if the mode changes
-  const theme = useMemo(() => createTheme(paletteProvider(themeState)), [themeState]);
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
+    <Provider store={store}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </ThemeProvider>
+    </Provider>
   );
 }

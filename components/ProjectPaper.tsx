2e2b4b0f52960be7333b8038d2c3189f6b1944c2
JKapturkiewicz <kuba.kapek@gmail.com>
2023-05-18 21:41:50 +0200

    Add skeleton loader

--- components/ProjectPaper.tsx
+++ components/ProjectPaper.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import Badge from "@mui/material/Badge";
 import Chip from "@mui/material/Chip";
-import { Box } from "@mui/material";
+import { Box, Skeleton } from "@mui/material";
 import Image from "next/image";
 import { Project, Locale } from "@/interfaces/projects";
 import { useRouter } from "next/router";
@@ -32,11 +32,11 @@ type Props = {
 //Styles
 const styles = {
   description: {
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    display: '-webkit-box',
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    display: "-webkit-box",
     WebkitLineClamp: 2,
-    WebkitBoxOrient: 'vertical',
+    WebkitBoxOrient: "vertical",
   },
   techIcon: {
     width: 14,
@@ -47,6 +47,15 @@ const styles = {
 const ProjectPaper = ({ project }: Props): JSX.Element => {
   const router = useRouter();
   const locale = (router.locale || "en") as Locale;
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulating data loading delay
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
 
   const getIcon = (tech: string): JSX.Element | null => {
     switch (tech) {
@@ -88,38 +97,57 @@ const ProjectPaper = ({ project }: Props): JSX.Element => {
   };
 
   return (
-    <Paper elevation={1} sx={{ p: 2, borderRadius: 2 }} className="pointer zoom">
-      <Box sx={{ position: "relative", width: "100%", height: "150px" }}>
-        <Image src={project.image} alt={project.title[locale]} fill style={{ objectFit: "cover", borderRadius: 4 }} />
-      </Box>
-      <Typography variant="h6" sx={{ mt: 2 }}>
-        {project.title[locale]}
-      </Typography>
+    <>
+      <Paper elevation={1} sx={{ p: 2, borderRadius: 2, minHeight: "320px" }} className="pointer zoom">
+        {loading ? (
+          <>
+            <Skeleton variant="rectangular" width="100%" height={150} style={{borderRadius: 4}} />
+            <Skeleton variant="text" width="50%" height={34} sx={{ mt: 2 }} />
+            <Skeleton variant="text" width="75%" height={12} sx={{ mt: 1 }} />
+            <Skeleton variant="text" width="75%" height={12} sx={{ mt: 1 }} />
+            <Skeleton variant="text" height={24} sx={{ mt: 1 }} />
+          </>
+        ) : (
+          <>
+            <Box sx={{ position: "relative", width: "100%", height: 150 }}>
+              <Image
+                src={project.image}
+                alt={project.title[locale]}
+                fill
+                style={{ objectFit: "cover", borderRadius: 4 }}
+              />
+            </Box>
+            <Typography variant="h6" sx={{ mt: 2 }}>
+              {project.title[locale]}
+            </Typography>
 
-      <Typography variant="body2" sx={styles.description} component="div">
-        {project.description[locale]}
-      </Typography>
+            <Typography variant="body2" sx={styles.description} component="div">
+              {project.description[locale]}
+            </Typography>
 
-      <Stack direction="row" spacing={1} sx={{ mt: 2 }}>
-        {project.technology.slice(0, 3).map((item, i) => (
-          <Badge
-            key={i}
-            badgeContent={
-              <Box sx={{ width: 14, height: 14, borderRadius: "50%", backgroundColor: "background.default" }}>
-                {getIcon(item)}
-              </Box>
-            }
-            // overlap="circular"
-            anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "right",
-            }}
-          >
-            <Chip label={item} size="small" />
-          </Badge>
-        ))}
-      </Stack>
-    </Paper>
+            <Stack direction="row" spacing={1} sx={{ mt: 2 }}>
+              {project.technology.slice(0, 3).map((item, i) => (
+                <Badge
+                  key={i}
+                  badgeContent={
+                    <Box sx={{ width: 14, height: 14, borderRadius: "50%", backgroundColor: "background.default" }}>
+                      {getIcon(item)}
+                    </Box>
+                  }
+                  // overlap="circular"
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "right",
+                  }}
+                >
+                  <Chip label={item} size="small" />
+                </Badge>
+              ))}
+            </Stack>
+          </>
+        )}
+      </Paper>
+    </>
   );
 };
 

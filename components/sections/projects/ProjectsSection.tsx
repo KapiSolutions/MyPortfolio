42303984d0539063899323ae79b89ed30f834bf8
JKapturkiewicz <kuba.kapek@gmail.com>
2023-05-19 09:11:42 +0200

    Add translations

--- components/sections/projects/ProjectsSection.tsx
+++ components/sections/projects/ProjectsSection.tsx
@@ -5,6 +5,8 @@ import { Projects } from "@/interfaces/projects";
 import MobileView from "./MobileView";
 import DesktopView from "./DesktopView";
 import TechCarousel from "./TechCarousel";
+import { useRouter } from "next/router";
+import { Locale } from "@/interfaces/main";
 
 //Define Types
 type Props = {
@@ -12,19 +14,26 @@ type Props = {
 };
 
 const ProjectsSection = ({ projects }: Props): JSX.Element => {
+  const router = useRouter();
+  const locale = (router.locale || "en") as Locale;
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"), {
     defaultMatches: true,
   });
 
+  const t = {
+    en: {
+      sectionName: "Projects",
+    },
+    pl: {
+      sectionName: "Projekty",
+    },
+  };
   return (
     <Box sx={{ width: "100vw", minHeight: "90vh" }} name="projectsSection" component="section">
       <Container sx={{ minHeight: "70vh" }}>
-        <Typography variant="h3" sx={{ mb: 1 }}>
-          <Typography variant="h4" component="span" color="text.disabled">
-            my
-          </Typography>
-          Works
+        <Typography variant="h4" sx={{ mb: 1, mt: 1 }}>
+          {t[locale].sectionName}
         </Typography>
         <Box>{isMobile ? <MobileView projects={projects} /> : <DesktopView projects={projects} />}</Box>
       </Container>

87e523ae8e96ac1981530c9017ac395b73afe459
JKapturkiewicz <kuba.kapek@gmail.com>
2023-05-19 10:06:04 +0200

    Change form Gap to Stack and update slides handling

--- components/sections/projects/MobileView.tsx
+++ components/sections/projects/MobileView.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Grid, Button, Container } from "@mui/material";
+import { Button, Container, Stack } from "@mui/material";
 import ProjectPaper from "../../ProjectPaper";
 import { Projects } from "@/interfaces/projects";
 import { useRouter } from "next/router";
@@ -13,33 +13,34 @@ type Props = {
 const MobileView = ({ projects }: Props): JSX.Element => {
   const router = useRouter();
   const locale = (router.locale || "en") as Locale;
-  const [howMany, setHowMany] = useState(3);
+  const initSlides = 3;
+  const [howMany, setHowMany] = useState(initSlides);
 
   const t = {
     en: {
       showMore: "Show more",
+      hide: "Hide",
     },
     pl: {
       showMore: "Więcej",
+      hide: "Zwiń",
     },
+    default: {},
   };
   return (
-    <Grid container spacing={2}>
+    <Stack spacing={2}>
       {projects.slice(0, howMany).map((project, index) => (
-        <Grid item xs={12} sm={6} md={4} key={index}>
-          <ProjectPaper project={project} />
-        </Grid>
+        <ProjectPaper project={project} key={index} />
       ))}
-      <Container sx={{ display: "flex", justifyContent: "center", mt: 2 }}>
-        <Button
-          variant="text"
-          color="inherit"
-          onClick={() => setHowMany(howMany >= projects.length ? projects.length : howMany + 2)}
-        >
-          {t[locale]?.showMore}
-        </Button>
-      </Container>
-    </Grid>
+
+      <Button
+        variant="text"
+        color="inherit"
+        onClick={() => setHowMany(howMany >= projects.length ? initSlides : howMany + 2)}
+      >
+        {howMany >= projects.length ? t[locale].hide : t[locale].showMore}
+      </Button>
+    </Stack>
   );
 };
 

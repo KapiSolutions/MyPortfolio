37c6508892e52e226a5926e0dfef13dd8998420f
JKapturkiewicz <kuba.kapek@gmail.com>
2023-05-23 20:36:51 +0200

    Use data set to create carousel slides

--- components/sections/bookBox/BookBoxSection.tsx
+++ components/sections/bookBox/BookBoxSection.tsx
@@ -1,60 +1,9 @@
 import React from "react";
-import { useRouter } from "next/router";
-import { Box, Container, Typography, Button, Stack, useTheme, useMediaQuery } from "@mui/material";
-import { Locale } from "@/interfaces/main";
-import img from "../../public/img/bookbox-section/1.png";
-import Image from "next/image";
-import Link from "next/link";
 import Carousel from "react-material-ui-carousel";
+import CarouselSlide from "./CarouselSlide";
+import slides from "./bookBoxSlides.json";
 
 const BookBoxSection = (): JSX.Element => {
-  const router = useRouter();
-  const locale = (router.locale || "en") as Locale;
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"), {
-    defaultMatches: true,
-  });
-  const t = {
-    en: {
-      h1: "BookBox Library",
-      p1: "The family of products tailored to the needs of both libraries and their users",
-      b1: "Online Catalogue",
-      h2: "BookBox Library",
-      p2: "Social Links",
-      b2: "first link",
-    },
-    pl: {
-      h1: "BookBox Library",
-      p1: "Rodzina produktów dostosowana do potrzeb bibliotek oraz czytelników",
-      b1: "Katalog online",
-      h2: "BookBox Library",
-      p2: "Social Links",
-      b2: "first link",
-    },
-    default: {},
-  };
-
-  const styles = {
-    wrapper: {
-      position: "relative",
-      minHeight: "100vh",
-      minWidth: "100vw",
-      display: "flex",
-      alignItems: "end",
-      justifyContent: "left",
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-    },
-    overlay: {
-      position: "absolute",
-      top: 0,
-      left: 0,
-      width: "100%",
-      height: "100%",
-      background: "linear-gradient(to bottom, transparent, rgba(0, 0, 0, 0.9))",
-    },
-  };
   return (
     <Carousel
       autoPlay={true}
@@ -63,6 +12,7 @@ const BookBoxSection = (): JSX.Element => {
       navButtonsAlwaysVisible={false}
       animation="fade"
       swipe={false}
+      interval={6000}
       duration={400}
       sx={{ m: "auto", height: "100vh", width: "100vw" }}
       indicatorContainerProps={{
@@ -78,57 +28,9 @@ const BookBoxSection = (): JSX.Element => {
         },
       }}
     >
-      <Box
-        sx={styles.wrapper}
-        style={{ backgroundImage: `url("img/bookbox-section/slide1${isMobile ? "mobile" : ""}.png")` }}
-      >
-        <Box sx={styles.overlay} />
-        <Container sx={{ zIndex: 1, mb: 10 }}>
-          <Stack spacing={2}>
-            <Typography variant="h4" component="h1">
-              BookBox Library
-            </Typography>
-            <Typography variant="body1">
-              The family of products tailored to the needs of both libraries and their users
-            </Typography>
-            <Link
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://drive.google.com/file/d/104EoBYCOkScEQ_DSHjN-dfhq0YiIOKIy/view?usp=sharing"
-            >
-              <Button variant="outlined" color="inherit">
-                Online Catalogue
-              </Button>
-            </Link>
-          </Stack>
-        </Container>
-      </Box>
-
-      <Box
-        sx={styles.wrapper}
-        style={{ backgroundImage: `url("img/bookbox-section/slide2${isMobile ? "mobile" : ""}.jpg")` }}
-      >
-        <Box sx={styles.overlay} />
-        <Container sx={{ zIndex: 1, mb: 10 }}>
-          <Stack spacing={2}>
-            <Typography variant="h4" component="h1">
-              BookBox Library cdn
-            </Typography>
-            <Typography variant="body1">
-              The family of products tailored to the needs of both libraries and their users
-            </Typography>
-            <Link
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://drive.google.com/file/d/104EoBYCOkScEQ_DSHjN-dfhq0YiIOKIy/view?usp=sharing"
-            >
-              <Button variant="outlined" color="inherit">
-                Online Catalogue
-              </Button>
-            </Link>
-          </Stack>
-        </Container>
-      </Box>
+      {slides.map((slide, idx) => (
+        <CarouselSlide key={idx} slide={slide} />
+      ))}
     </Carousel>
   );
 };
---
+++ components/sections/bookBox/CarouselSlide.tsx
@@ -0,0 +1,106 @@
+import React from "react";
+import { useRouter } from "next/router";
+import { Box, Container, Typography, Button, Stack, useTheme, useMediaQuery } from "@mui/material";
+import { Locale } from "@/interfaces/main";
+import img from "../../public/img/bookbox-section/1.png";
+import Image from "next/image";
+import Link from "next/link";
+
+//Define Types:
+type Lang = {
+  en: string | Array<string>;
+  pl: string | Array<string>;
+};
+type Button = {
+  text: Lang;
+  url: string;
+  newPage: boolean;
+};
+interface Slide {
+  title: Lang;
+  desc: Lang;
+  buttons: Array<Button>;
+  images: {
+    desktop: string;
+    mobile: string;
+  };
+}
+type Props = {
+  slide: Slide;
+};
+
+const CarouselSlide = ({ slide }: Props): JSX.Element => {
+  const router = useRouter();
+  const locale = (router.locale || "en") as Locale;
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"), {
+    defaultMatches: true,
+  });
+
+  const styles = {
+    wrapper: {
+      position: "relative",
+      minHeight: "100vh",
+      minWidth: "100vw",
+      display: "flex",
+      alignItems: "end",
+      justifyContent: "left",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+    },
+    overlay: {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      background: `linear-gradient(180deg, transparent 50%, ${theme.palette.background.default} 100%)`,
+    },
+  };
+  return (
+    <Box
+      sx={styles.wrapper}
+      style={{ backgroundImage: `url("${isMobile ? slide.images.mobile : slide.images.desktop}")` }}
+    >
+      <Box sx={styles.overlay} />
+      <Box
+        sx={{
+          zIndex: 1,
+          mb: 6,
+          p: 3,
+          width: "100%",
+          background: `linear-gradient(330deg, transparent ${isMobile ? "" : "20%"}, ${
+            theme.palette.background.default
+          } 100%)`,
+        }}
+      >
+        <Stack spacing={2} sx={{ width: isMobile ? "100%" : "60%" }}>
+          <Typography variant="h4" component="h1">
+            {slide.title[locale]}
+          </Typography>
+
+          <Typography variant="body1">{slide.desc[locale]}</Typography>
+
+          {/* Actions */}
+          <Stack direction="row" justifyContent="flex-start" alignItems="center" useFlexGap flexWrap="wrap" spacing={2}>
+            {slide.buttons.map((button, idx) => (
+              <Link
+                target={button.newPage ? "_blank" : "_self"}
+                rel={button.newPage ? "noopener noreferrer" : "next"}
+                href={button.url}
+                key={idx}
+              >
+                <Button variant="contained" size={slide.buttons.length > 1 ? "small" : "medium"} color="primary">
+                  {button.text[locale]}
+                </Button>
+              </Link>
+            ))}
+          </Stack>
+        </Stack>
+      </Box>
+    </Box>
+  );
+};
+
+export default CarouselSlide;

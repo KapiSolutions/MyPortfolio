c9b18ef324f3237c988196cab0d36e801fad8cf7
JKapturkiewicz <kuba.kapek@gmail.com>
2023-05-25 09:26:50 +0200

    Auth test

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,16 +1,16 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { UserProfile } from "@auth0/nextjs-auth0/client";
+import { Typography } from "@mui/material";
+import Link from "next/link";
 
 type ProfileProps = { user: UserProfile };
 
 export default function AdminPage({ user }: ProfileProps): JSX.Element {
-    // console.log(user);
+  // console.log(user);
   return (
-    <>
-      <h3>
-           Welcome {user.name}! <a href="/api/auth/logout"> - Logout</a>
-      </h3>
-    </>
+    <Typography variant="body1" sx={{m:4, mt:0}}>
+      Welcome {user.name}! <Link href="/api/auth/logout"> - Logout</Link>
+    </Typography>
   );
 }
 
@@ -18,7 +18,7 @@ export const getServerSideProps = withPageAuthRequired({
   // withPageAuthRequired checks if the session is authenticated, if not then redirect to Auth0 login page
   async getServerSideProps(ctx) {
     // const session = await getSession(ctx.req, ctx.res);
-    
+
     // custom props are augmented with the session user object
     return {
       props: {

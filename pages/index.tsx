b41d9cae53fc03d1e57856290a74ad6f89a99663
JKapturkiewicz <kuba.kapek@gmail.com>
2023-05-25 19:33:47 +0200

    Remove margin and padding from the Stack wrapper

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Stack direction="column" spacing={3} justifyContent="center" alignItems="center" sx={{ pt: 2, pb: 4}} >
+      <Stack direction="column" spacing={3} justifyContent="center" alignItems="center" >
         <ProjectsSection projects={projects}/>
         <BookBoxSection />
         <CarrierSection carrier={carrier}/>

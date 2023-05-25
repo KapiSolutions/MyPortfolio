bdab5639dfda46a583d5936bae6660a71b5fa1c3
JKapturkiewicz <kuba.kapek@gmail.com>
2023-05-25 15:32:13 +0200

    Change header Typography variant

--- components/sections/projects/ProjectsSection.tsx
+++ components/sections/projects/ProjectsSection.tsx
@@ -33,7 +33,7 @@ const ProjectsSection = ({ projects }: Props): JSX.Element => {
   return (
     <Box sx={{ width: "100vw", minHeight: "90vh" }} name="projectsSection" component="section">
       <Container sx={{ minHeight: "70vh" }}>
-        <Typography variant="h3" sx={{ mb: 1, mt: 1 }}>
+        <Typography variant="h2" sx={{ mb: isMobile ? 2 : 1, mt: 1 }}>
           {t[locale].sectionName}
         </Typography>
         <Box>{isMobile ? <MobileView projects={projects} /> : <DesktopView projects={projects} />}</Box>

20a2fcc3ed736c326853c73b274f377f2553629c
JKapturkiewicz <kuba.kapek@gmail.com>
2023-05-25 09:20:55 +0200

    Add Auth0 wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "@/styles/main.scss";
 import "@fontsource/k2d";
 import Layout from "../components/layout/Layout";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { Provider } from "react-redux";
 import { store } from "../store/store";
 
@@ -10,10 +11,12 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
+    <UserProvider>
+      <Provider store={store}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
+    </UserProvider>
   );
 }

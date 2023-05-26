051a73ac10441765ead29d681e7b80ccf662e982
JKapturkiewicz <kuba.kapek@gmail.com>
2023-05-26 20:52:56 +0200

    Add map func with ProjectItem component

--- pages/admin/projects/index.tsx
+++ pages/admin/projects/index.tsx
@@ -1,16 +1,56 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { UserProfile } from "@auth0/nextjs-auth0/client";
-import { Typography, Container, useTheme, useMediaQuery } from "@mui/material";
+import { Typography, Container, Stack, Grid, Divider, Box, Button, useTheme, useMediaQuery } from "@mui/material";
+import PostAddIcon from "@mui/icons-material/PostAdd";
+import ProjectItem from "@/components/admin/projects/ProjectItem";
+import projects from "@/tmp/projects.json"; //!Temporary import
+import Link from "next/link";
 
 type ProfileProps = { user: UserProfile };
 
 export default function AdminProjectsPage({ user }: ProfileProps): JSX.Element {
-  // console.log(user);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"), {
+    defaultMatches: true,
+  });
   return (
     <Container>
-      <Typography variant="h2">
-        Admin-projects
+      <Typography variant="h4" align={isMobile ? "center" : "left"}>
+        Menage projects
       </Typography>
+      <Box display="flex" justifyContent="flex-end" sx={{ mt: 4 }}>
+        <Link href="/admin/projects/new#main">
+          <Button variant="outlined" color="inherit" size="large" sx={{ width: isMobile ? "100%" : "auto" }}>
+            <PostAddIcon sx={{ mr: 1 }} />
+            Add New!
+          </Button>
+        </Link>
+      </Box>
+
+      {/* Title Bar */}
+      <Grid container spacing={2} wrap="nowrap" sx={{ mt: 2 }}>
+        <Grid item xs={9} sm={5} lg={9}>
+          <Typography variant="body1">Title</Typography>
+        </Grid>
+        {isMobile ? null : (
+          <Grid item sm={3} lg={1}>
+            <Typography variant="body1">Number</Typography>
+          </Grid>
+        )}
+        <Grid item xs={3} sm={4} lg={2}>
+          <Box display="flex">
+            <Typography variant="body1">Opcje</Typography>
+          </Box>
+        </Grid>
+      </Grid>
+
+      {/* Items */}
+      <Stack direction="column" spacing={3} sx={{ mt: 2 }}>
+        <Divider orientation="horizontal" flexItem />
+        {projects.map((project, idx) => (
+          <ProjectItem key={idx} project={project} />
+        ))}
+      </Stack>
     </Container>
   );
 }

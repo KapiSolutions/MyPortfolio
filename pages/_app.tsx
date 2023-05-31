cc50651038d849dac60f19abd29cf54eb8fc1902
JKapturkiewicz <kuba.kapek@gmail.com>
2023-05-31 14:27:54 +0200

    Add SnackbarProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "@/styles/main.scss";
 import "@fontsource/k2d";
 import Layout from "../components/layout/Layout";
+import { SnackbarProvider } from 'notistack';
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { Provider } from "react-redux";
 import { store } from "../store/store";
@@ -13,9 +14,11 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
       <Provider store={store}>
+      <SnackbarProvider maxSnack={3}>
         <Layout>
           <Component {...pageProps} />
         </Layout>
+        </SnackbarProvider>
       </Provider>
     </UserProvider>
   );

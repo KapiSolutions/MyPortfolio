541ed68dd13171fecc81c0822a14d620b983dbbb
JKapturkiewicz <kuba.kapek@gmail.com>
2023-06-01 18:00:27 +0200

    Add router push with query

--- components/admin/projects/ProjectItem.tsx
+++ components/admin/projects/ProjectItem.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Button, Box, Grid, Stack, Typography, Divider, useTheme, useMediaQuery } from "@mui/material";
-import { Project } from "@/interfaces/projects";
+import type { Project } from "@/schema/project";
 import { useRouter } from "next/router";
 import { Locale } from "@/interfaces/main";
 
@@ -22,41 +22,56 @@ const ProjectItem = ({ project }: Props): JSX.Element => {
     en: {
       showMore: "Show more",
       hide: "Hide",
+      deleteButton: "Delete",
+      editButton: "Edit",
     },
     pl: {
       showMore: "Więcej",
       hide: "Zwiń",
+      deleteButton: "Usuń",
+      editButton: "Edytuj",
     },
     default: {},
   };
   return (
     <Stack direction="column" spacing={3}>
-    <Grid container spacing={2} wrap="nowrap" direction="row" justifyContent="center">
-      <Grid item xs={9} sm={5} lg={9}>
-        <Typography variant="body1">{project.title[locale]}</Typography>
-      </Grid>
-
-      {isMobile ? null : (
-        <Grid item sm={3} lg={1}>
-          <Typography variant="body1">Number</Typography>
+      <Grid container spacing={2} wrap="nowrap" direction="row" justifyContent="center">
+        <Grid item xs={9} sm={5} lg={9}>
+          <Typography variant="body1">{project.title[locale]}</Typography>
         </Grid>
-      )}
 
-      <Grid item xs={3} sm={4} lg={2}>
-        <Stack direction="row" spacing={3} >
-          {isMobile ? null : (
-            <Button variant="outlined" size="small" color="error">
-              Delete
+        {isMobile ? null : (
+          <Grid item sm={3} lg={1}>
+            <Typography variant="body1">{project.date}</Typography>
+          </Grid>
+        )}
+
+        <Grid item xs={3} sm={4} lg={2}>
+          <Stack direction="row" spacing={3}>
+            {isMobile ? null : (
+              <Button variant="outlined" size="small" color="error">
+                {t[locale].deleteButton}
+              </Button>
+            )}
+
+            <Button
+              variant="contained"
+              size="small"
+              onClick={() => {
+                router.push({
+                  pathname: "/admin/projects/[pid]",
+                  query: { pid: project.title.en.replaceAll(" ", "-"), id: project._id },
+                  hash: "main",
+                });
+                // setLoadingEdit(true);
+              }}
+            >
+              {t[locale].editButton}
             </Button>
-          )}
-
-          <Button variant="contained" size="small" >
-            Edit
-          </Button>
-        </Stack>
+          </Stack>
+        </Grid>
       </Grid>
-    </Grid>
-    <Divider orientation="horizontal" flexItem />
+      <Divider orientation="horizontal" flexItem />
     </Stack>
   );
 };

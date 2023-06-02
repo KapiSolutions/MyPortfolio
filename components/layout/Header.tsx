0990aa6e9b5fc7f2d030b20da1c47a1c073c9e03
JKapturkiewicz <kuba.kapek@gmail.com>
2023-06-02 15:30:56 +0200

    Update

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "../../styles/layout/Header.module.scss";
 import { useRouter } from "next/router";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Typography, Stack, useTheme, useMediaQuery } from "@mui/material";
 import AdbIcon from "@mui/icons-material/Adb";
 import Navbar from "./Navbar";
 import { Locale } from "@/interfaces/main";
@@ -13,14 +13,16 @@ type Props = {
 
 const Header = ({ locale }: Props): JSX.Element => {
   const router = useRouter();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"), {
+    defaultMatches: true,
+  });
   const t = {
     en: {
-      h: "I'm Kuba",
       p: "Full-Stack Developer & Mechatronic Systems Engineer",
       button: "Contact me",
     },
     pl: {
-      h: "Kuba",
       p: "Full-Stack Developer & Mechatronic Systems Engineer",
       button: "Contact me",
     },
@@ -28,31 +30,28 @@ const Header = ({ locale }: Props): JSX.Element => {
   return (
     <Box className={styles.container}>
       <Navbar />
-      <Box className={styles.parallaxContent}>
-        <Typography mb={2} variant="h2">
-          {t[locale]?.h}
-        </Typography>
-        <Typography variant="h6" mb={2}>
+      <Stack spacing={isMobile ? 3 : 6} className={styles.parallaxContent}>
+        <Typography variant={isMobile ? "h3" : "h2"}>Jakub Kapturkiewicz</Typography>
+        <Typography variant={isMobile ? "body1" : "h6"} mb={2}>
           {t[locale]?.p}
         </Typography>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => {
-            if (router.route === "/") {
-              document.getElementsByName("main")[0].scrollIntoView({ block: "start", inline: "nearest" });
-            } else {
-              router.push("/#main");
-              // document.getElementsByName("main")[0].scrollIntoView({ block: "start", inline: "nearest" });
-            }
-          }}
-        >
-          Contact Me
-        </Button>
-        <br />
-        <br />
-        <AdbIcon fontSize="large" />
-      </Box>
+        <Box>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              if (router.route === "/") {
+                document.getElementsByName("main")[0].scrollIntoView({ block: "start", inline: "nearest" });
+              } else {
+                router.push("/#main");
+                // document.getElementsByName("main")[0].scrollIntoView({ block: "start", inline: "nearest" });
+              }
+            }}
+          >
+            Explore Projects
+          </Button>
+        </Box>
+      </Stack>
     </Box>
   );
 };

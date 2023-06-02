ddd3143e84a233b3116918ed38ad4dee33c6bc75
JKapturkiewicz <kuba.kapek@gmail.com>
2023-06-02 13:32:02 +0200

    Add new type

--- interfaces/main.ts
+++ interfaces/main.ts
@@ -3,3 +3,10 @@ type Lang = {
   pl: string;
 };
 export type Locale = keyof Lang;
+
+type BreadCrumb = {
+  name: string;
+  path: string;
+};
+export interface BreadCrumbs extends Array<BreadCrumb> {}
+

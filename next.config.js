cd790ec68dadb7069f5067a85e0efc7ffe7e0ed8
JKapturkiewicz <kuba.kapek@gmail.com>
2023-06-05 15:23:16 +0200

    Add secure headers

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
+const prod = process.env.NODE_ENV == "production";
 const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "via.placeholder.com",
-      },
       {
         protocol: "https",
         hostname: "storage.googleapis.com",
@@ -20,6 +17,41 @@ const nextConfig = {
     localeDetection: false,
   },
   trailingSlash: false,
-}
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: `default-src 'self' https://vitals.vercel-insights.com https://www.google.com/recaptcha/ https://translate.googleapis.com ; form-action 'self'; frame-src 'self' https://www.google.com/recaptcha/; style-src 'self' https://fonts.googleapis.com 'unsafe-inline'; img-src 'self' https://storage.googleapis.com data: ; script-src 'self' ${
+              prod ? "" : "'unsafe-eval'"
+            } https://apis.google.com  https://vitals.vercel-insights.com/  https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/ ;`,
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

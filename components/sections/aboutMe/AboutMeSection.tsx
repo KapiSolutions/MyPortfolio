ca5564027fc005a22c625845970422b07417b22e
JKapturkiewicz <kuba.kapek@gmail.com>
2023-06-07 16:55:57 +0200

    Change images

--- components/sections/aboutMe/AboutMeSection.tsx
+++ components/sections/aboutMe/AboutMeSection.tsx
@@ -4,8 +4,8 @@ import { Container, Typography, Stack, Box, useTheme, useMediaQuery, Divider } f
 import { Locale } from "@/utils/interfaces/main";
 import sections from "./aboutMe.json";
 import Image from "next/image";
-import img1 from "@/public/img/aboutme-section/1.png";
-import img2 from "@/public/img/aboutme-section/2.png";
+import img1 from "@/public/img/aboutme-section/1c.png";
+import img2 from "@/public/img/aboutme-section/2a.png";
 import img3 from "@/public/img/aboutme-section/3.png";
 const images = [img1, img2, img3];
 
@@ -30,30 +30,30 @@ const AboutMeSection = (): JSX.Element => {
   return (
     <Container name="aboutMeSection">
       <Typography variant="h2">{t[locale]?.h}</Typography>
-      <Stack spacing={4} alignItems="center" sx={{ margin: "auto", mt: 2, width: isMobile ? "100%" : "75%" }}>
+      <Stack spacing={4} alignItems="center" sx={{ margin: "auto", mt: 2, width: isMobile ? "100%" : "90%" }}>
         {sections.map((item, idx) => (
           <Box key={idx} sx={{ width: "100%" }}>
             <Stack
               direction={isMobile ? "column" : isEven(idx + 1) ? "row-reverse" : "row"}
               justifyContent={isMobile ? "center" : isEven(idx + 1) ? "flex-end" : "flex-start"}
               alignItems="center"
-              spacing={2}
-              sx={{ width: "100%" }}
+              spacing={3}
+              sx={{ width: "100%", height: "100vh" }}
             >
               <Box
                 component="div"
                 sx={{
                   position: "relative",
-                  width: isMobile ? "100%" : "200px",
-                  height: isMobile ? "200px" : "270px",
+                  width: isMobile ? "100%" : "25%",
+                  height: isMobile ? "200px" : "70vh",
                   margin: isMobile ? "auto" : 0,
                 }}
               >
-                <Image src={images[idx]} alt="About me image" fill style={{ objectFit: "cover" }} />
+                <Image src={images[idx]} alt="About me image" fill style={{ objectFit: "cover", filter:"grayscale(50%) opacity(80%)"}} />
               </Box>
 
               <Stack spacing={2} sx={{ width: isMobile ? "100%" : "75%" }}>
-                <Typography variant="h4" align={isEven(idx + 1) ? (isMobile ? "left" : "right") : "left"}>
+                <Typography variant="h4" sx={{fontWeight: "bold", textTransform: "uppercase"}} align={isEven(idx + 1) ? (isMobile ? "left" : "right") : "left"}>
                   {item.title[locale]}
                 </Typography>
                 <Typography variant="body1" align="justify">

55f72006243400b03e658fa336b397b9915d880e
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-28 11:55:15 +0200

    Update buttons style and position

--- components/ImageGallery.tsx
+++ components/ImageGallery.tsx
@@ -22,22 +22,28 @@ const ImageGallery = ({ imgSet, show, setShow }: Props): JSX.Element => {
   const styles = {
     navIcons: {
       position: "absolute",
-      bgcolor: "rgba(255, 255, 255, 0.8)",
+      bgcolor: "common.white",
+      color: "common.black",
+      boxShadow: 1,
       "&:hover": {
-        bgcolor: "layout.dark",
-        color: "primary.main",
+        bgcolor: "common.white",
+        color: "common.black",
         transform: "scale(1.1)",
       },
     },
   };
-const closeGallery = ()=>{
+  const closeGallery = () => {
     setShow(-1);
     setImgNumber(-1);
-}
+  };
   return (
     <Backdrop sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }} open={show >= 0}>
-      <ClickAwayListener onClickAway={() => {imgNumber >= 0 && closeGallery()}}>
-        <Box sx={{ position: "relative", width: "90%", height: "90%", borderRadius: 8 }}>
+      <ClickAwayListener
+        onClickAway={() => {
+          imgNumber >= 0 && closeGallery();
+        }}
+      >
+        <Box sx={{ position: "relative", width: "80%", height: "90%", borderRadius: 8 }}>
           {imgSet[imgNumber] && (
             <Image
               src={imgSet[imgNumber]}
@@ -48,20 +54,7 @@ const closeGallery = ()=>{
             />
           )}
 
-          <IconButton
-            aria-label="close"
-            onClick={closeGallery}
-            sx={{
-              position: "absolute",
-              right: 10,
-              top: 10,
-              bgcolor: "rgba(255, 255, 255, 0.8)",
-              "&:hover": {
-                bgcolor: "layout.dark",
-                color: "primary.main",
-              },
-            }}
-          >
+          <IconButton aria-label="close" onClick={closeGallery} sx={{ ...styles.navIcons, right: -12, top: -12 }}>
             <CloseIcon />
           </IconButton>
           {imgNumber > 0 && (
@@ -69,7 +62,7 @@ const closeGallery = ()=>{
               aria-label="previous"
               size="large"
               onClick={() => setImgNumber(imgNumber - 1)}
-              sx={{ ...styles.navIcons, left: 10, top: "50%" }}
+              sx={{ ...styles.navIcons, left: -70, top: "50%" }}
             >
               <ArrowForwardIosIcon sx={{ transform: "rotate(180deg)" }} />
             </IconButton>
@@ -79,7 +72,7 @@ const closeGallery = ()=>{
               aria-label="next"
               size="large"
               onClick={() => setImgNumber(imgNumber + 1)}
-              sx={{ ...styles.navIcons, right: 10, top: "50%" }}
+              sx={{ ...styles.navIcons, right: -70, top: "50%" }}
             >
               <ArrowForwardIosIcon />
             </IconButton>

afb46ba184baa8466c5eb5152ab779e0a12e6b3c
JKapturkiewicz <kuba.kapek@gmail.com>
2025-02-04 08:53:36 +0100

    chore: remove comments

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,19 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from "next/server";
 
-const PUBLIC_FILE = /\.(.*)$/
+const PUBLIC_FILE = /\.(.*)$/;
 
 export async function middleware(req: NextRequest) {
   if (
-    req.nextUrl.pathname.startsWith('/_next') ||
-    req.nextUrl.pathname.includes('/api/') ||
+    req.nextUrl.pathname.startsWith("/_next") ||
+    req.nextUrl.pathname.includes("/api/") ||
     req.headers.has("x-prerender-revalidate") || // exclude the revalidate request
     PUBLIC_FILE.test(req.nextUrl.pathname)
   ) {
-    return
+    return;
   }
-  if (req.nextUrl.locale === 'default') {
+  if (req.nextUrl.locale === "default") {
     const userLang = req.headers.get("accept-language")?.split(",")[0].trim();
-    // const locale = req.cookies.get('NEXT_LOCALE')?.value || 'pl'
     const locale = userLang === "pl" ? "pl" : "en";
-    return NextResponse.redirect(
-      new URL(`/${locale}${req.nextUrl.pathname}${req.nextUrl.search}`, req.url)
-    )
+    return NextResponse.redirect(new URL(`/${locale}${req.nextUrl.pathname}${req.nextUrl.search}`, req.url));
   }
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,22 +2,20 @@ import React from "react";
 import "@/styles/main.scss";
 import "@fontsource/k2d";
 import Layout from "../components/layout/Layout";
-import { SnackbarProvider } from 'notistack';
+import { SnackbarProvider } from "notistack";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { Provider } from "react-redux";
 import { store } from "../store/store";
-
-// Import types
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
       <Provider store={store}>
-      <SnackbarProvider maxSnack={3}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <SnackbarProvider maxSnack={3}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
         </SnackbarProvider>
       </Provider>
     </UserProvider>

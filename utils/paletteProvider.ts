5c171e3a13e503d3367e1619bed8491db1e16728
JKapturkiewicz <kuba.kapek@gmail.com>
2025-02-04 09:21:34 +0100

    chore: update palette

--- utils/paletteProvider.ts
+++ utils/paletteProvider.ts
@@ -1,5 +1,4 @@
 import { PaletteMode } from "@mui/material";
-// import { amber, deepOrange, grey } from "@mui/material/colors";
 
 const paletteProvider = (mode: PaletteMode) => ({
   typography: {
@@ -9,43 +8,38 @@ const paletteProvider = (mode: PaletteMode) => ({
     mode,
     ...(mode === "light"
       ? {
-          // palette values for light mode
+          //Light mode
           divider: "rgba(0, 0, 0, 0.12)",
           background: {
             default: "#fafafa",
             paper: "#fff",
             nav: " rgba(255, 255, 255, 0.75)",
-          },
-          primary: {
-            main: "#121212",
-            dark: "#000000",
+            contrast: "rgba(255, 255, 255, 0.75)",
           },
           text: {
             primary: "rgba(0, 0, 0, 0.87)",
             secondary: "rgba(0, 0, 0, 0.6)",
             disabled: "rgba(0, 0, 0, 0.38)",
           },
+          primary: {
+            main: "#121212",
+            dark: "#000000",
+          },
         }
       : {
-          // palette values for dark mode
+          //Dark mode
           divider: "rgba(255, 255, 255, 0.12)",
           background: {
             default: "#121212",
             paper: "#121212",
             nav: "rgba(18, 18, 18, 0.75)",
+            contrast: "rgb(0, 0, 0, 0.9)",
           },
           text: {
             primary: "#fff",
             secondary: "rgba(255, 255, 255, 0.7)",
             disabled: "rgba(255, 255, 255, 0.5)",
           },
-          // action: {
-          //   active: "#fff",
-          //   hover: "rgba(255, 255, 255, 0.08)",
-          //   selected: "rgba(255, 255, 255, 0.16)",
-          //   disabled: "rgba(255, 255, 255, 0.3)",
-          //   disabledBackground: "rgba(255, 255, 255, 0.12)",
-          // },
         }),
   },
 });
